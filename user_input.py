8482794c40f4e84b338734503a89c62e6fbdc053
Alisonrajpal <135074591+Alisonrajpal@users.noreply.github.com>
2024-05-25 05:09:48 +0200

    Create user.py

---
+++ user_input.py
@@ -0,0 +1,6 @@
+name =input("What is your name ")
+age=input("What is your age ")
+location = input ("what is your location ")
+print('Welcome,'+name+ age +location)
+print("Hello "+name+ ",you are " +age + "years old and live in "+location)
+      
